36e2a1742b878f22bf2346aaa61c8667dc9fd94b
NavneetSingh <43246502+josannavnee@users.noreply.github.com>
2019-01-14 16:55:09 -0500

    week 2 class work

---
+++ Script1.js
@@ -0,0 +1,13 @@
+// Name: Navneet Singh
+// Date: January 14,2019
+// Student: C0749303
+// Course: CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
